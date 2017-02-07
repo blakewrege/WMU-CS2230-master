7995ae20e368b0d364ae51bd1e034a371708d6af
blake <Blakewrege1@gmail.com>
2017-02-07 17:06:16 -0500

    Update rgb.c

--- rgb/rgb.c
+++ rgb/rgb.c
@@ -1,6 +1,9 @@
 #include <msp430.h>
-// Note: 0b00000001 = BIT0
 
+// If the char entered is 1 it turns on that LED segment
+// RED -> P1.1 -> BIT1
+// GREEN -> P1.2 -> BIT2
+// Blue -> P1.3 -> BIT3
 void rgb(char RED, char GREEN, char BLUE) {
   P1OUT = RED << 1 | GREEN << 2 | BLUE << 3;
 }
@@ -19,20 +22,28 @@ int main(void) {
 
 // Infinite loop changing the LED color
   for (;;) {
+    // none = no color
     rgb(0, 0, 0);
     __delay_cycles(1000000);
+    // BIT1 = RED
     rgb(1, 0, 0);
     __delay_cycles(1000000);
+    // BIT1+BIT2 = RED+GREEN = YELLOW
     rgb(1, 1, 0);
     __delay_cycles(1000000);
+    // BIT2 = GREEN
     rgb(0, 1, 0);
     __delay_cycles(1000000);
+    // BIT2+BIT3 = GREEN+BLUE = CYAN
     rgb(0, 1, 1);
     __delay_cycles(1000000);
+    // BIT3 = BLUE
     rgb(0, 0, 1);
     __delay_cycles(1000000);
+    // BIT1+BIT3 = RED+BLUE = PURPLE
     rgb(1, 0, 1);
     __delay_cycles(1000000);
+    // BIT1+BIT2+BIT3 = RED+GREEN+BLUE = WHITE
     rgb(1, 1, 1);
     __delay_cycles(1000000);
 
