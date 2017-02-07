4f1c8d4de2eb08cee67272242dcdbfbf759f6aac
blake wrege <blakewrege1@gmail.com>
2017-02-07 16:39:25 -0500

    added rgb tutorial

---
+++ rgb/rgb.c
@@ -0,0 +1,41 @@
+#include <msp430.h>
+// Note: 0b00000001 = BIT0
+
+void rgb(char RED, char GREEN, char BLUE) {
+  P1OUT = RED << 1 | GREEN << 2 | BLUE << 3;
+}
+
+int main(void) {
+
+  // Stop watchdog timer to prevent time out reset
+  WDTCTL = WDTPW | WDTHOLD;
+  // Sets Digitally Controlled Oscilator(DCO) range to 1MHZ
+  BCSCTL1 = CALBC1_1MHZ;
+  DCOCTL = CALDCO_1MHZ;
+
+// Sets the P1.0 direction to output
+  P1DIR = BIT1+BIT2+BIT3;
+  //P1DIR = 0b00001110;
+
+// Infinite loop turning the LED off and on
+  for (;;) {
+    rgb(0, 0, 0);
+    __delay_cycles(1000000);
+    rgb(1, 0, 0);
+    __delay_cycles(1000000);
+    rgb(1, 1, 0);
+    __delay_cycles(1000000);
+    rgb(0, 1, 0);
+    __delay_cycles(1000000);
+    rgb(0, 1, 1);
+    __delay_cycles(1000000);
+    rgb(0, 0, 1);
+    __delay_cycles(1000000);
+    rgb(1, 0, 1);
+    __delay_cycles(1000000);
+    rgb(1, 1, 1);
+    __delay_cycles(1000000);
+
+  }
+  return 0;
+}
